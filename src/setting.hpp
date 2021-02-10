f59d381c9e134580bd9c38fbb6db452c47229123
artempelevin <artempelevin@list.ru>
2021-02-10 22:43:52 +0300

    Changed the loyout of the  elements on the screen

--- src/setting.hpp
+++ src/setting.hpp
@@ -21,26 +21,26 @@
 #define BUTTON_X            0
 #define BUTTON_Y            SCREEN_HEIGHT - BUTTON_HEIGHT
 
-#define PREV_BUTTON_WIDTH   200
-#define PREV_BUTTON_HEIGHT  40
-#define PREV_BUTTON_X       0
-#define PREV_BUTTON_Y       0
-
-#define BUTTON_INDENT       5
-#define NEXT_BUTTON_WIDTH   PREV_BUTTON_WIDTH
-#define NEXT_BUTTON_HEIGHT  PREV_BUTTON_HEIGHT
-#define NEXT_BUTTON_X       PREV_BUTTON_WIDTH + BUTTON_INDENT
-#define NEXT_BUTTON_Y       PREV_BUTTON_Y
-
 #define TASK_WIDTH          300
-#define TASK_HEIGHT         500
+#define TASK_HEIGHT         350
 #define TASK_X              SCREEN_WIDTH  - (TASK_WIDTH + 20)
 #define TASK_Y              10
 
+#define BUTTON_INDENT       6
+#define PREV_BUTTON_WIDTH   TASK_WIDTH/2 - BUTTON_INDENT/2
+#define PREV_BUTTON_HEIGHT  35
+#define PREV_BUTTON_X       TASK_X
+#define PREV_BUTTON_Y       TASK_Y + TASK_HEIGHT + BUTTON_INDENT
+
+#define NEXT_BUTTON_WIDTH   PREV_BUTTON_WIDTH
+#define NEXT_BUTTON_HEIGHT  PREV_BUTTON_HEIGHT
+#define NEXT_BUTTON_X       PREV_BUTTON_X + PREV_BUTTON_WIDTH + BUTTON_INDENT
+#define NEXT_BUTTON_Y       PREV_BUTTON_Y
+
 #define KID_WIDTH           200
 #define KID_HEIGHT          350
-#define KID_X               200
-#define KID_Y               200
+#define KID_X               30
+#define KID_Y               BUTTON_WIDTH + 10
 
 #define GIFT_WIDTH          BUTTON_WIDTH
 #define GIFT_HEIGHT         BUTTON_HEIGHT
